876c9d24471ad051f1a68c7f1a067054d4ee1456
PJMantoss <joelptoss@gmail.com>
2022-07-30 20:49:38 +0100

    update App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import logo from './logo.svg';
+import Info from './components/Info';
+import About from './components/About';
+import Interests from './components/Interests';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
