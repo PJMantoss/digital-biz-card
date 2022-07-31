1206f05237809c5cbd8e154448a019cd55461e7e
PJMantoss <joelptoss@gmail.com>
2022-07-31 16:51:01 +0100

    update Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -7,7 +7,11 @@ const Info = () => {
       <di className='userInfo'>
         <h2 className='name'>PJ Mantoss</h2>
         <p className='position'>Frontend Developer</p>
-        <span className='website'>https://pjmantoss.github.io/</span>
+        <a 
+          href='https://pjmantoss.github.io/' 
+          target='_blank' 
+          className='website'
+        >https://pjmantoss.github.io/</a>
       </di>
       <div className='socialMedia'>
         <button className='emailBtn'>Email</button>
