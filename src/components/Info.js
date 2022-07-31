55a83b8a05c5d462d61ebf2565692fad26c9fb8e
PJMantoss <joelptoss@gmail.com>
2022-07-31 17:24:43 +0100

    update Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -17,7 +17,11 @@ const Info = () => {
         <a href='#' className='emailBtn'>
           <img src='./images/email-icon.png' alt='email-icon'/> Email
           </a>
-        <a href='#' className='linkedInBtn'>
+        <a 
+          href='https://www.linkedin.com/in/pj-mantoss-55a372a3/' 
+          target='_blank'
+          className='linkedInBtn'
+        >
           <img src='./images/linkedin-icon.png' alt='email-icon'/> LinkedIn
         </a>
       </div>
