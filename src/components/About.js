27f3b8fd98a354f9c14d2f9d31b2d43e4d319894
PJMantoss <joelptoss@gmail.com>
2022-07-31 17:33:10 +0100

    update styling

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='about'>
+      <h3>About</h3>
+      <p>I am a frontend developer with a particular interest
+        in making things simple and automating daily tasks. I try
+        to keep up with security and best practices, and I'm
+        always looking for new things to learn. 
+      </p>
+    </div>
   )
 }
 
