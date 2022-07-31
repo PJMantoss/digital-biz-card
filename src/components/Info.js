fb1a3e59b4cfb255a4c37df60419866a43fd5620
PJMantoss <joelptoss@gmail.com>
2022-07-31 15:35:03 +0100

    update Info

--- src/components/Info.js
+++ src/components/Info.js
@@ -4,8 +4,15 @@ const Info = () => {
   return (
     <div className='info'>
       <img src='./images/profile_pic.jpg' alt='user image' />
-      <di className='username'></di>
-      <div className='socialMedia'></div>
+      <di className='userInfo'>
+        <h2 className='name'>PJ Mantoss</h2>
+        <p className='position'>Frontend Developer</p>
+        <span className='website'>https://pjmantoss.github.io/</span>
+      </di>
+      <div className='socialMedia'>
+        <button className='emailBtn'>Email</button>
+        <button className='linkedInBtn'>LinkedIn</button>
+      </div>
     </div>
   )
 }
